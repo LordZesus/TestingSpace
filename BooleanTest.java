a9b8e72491c0523667778816bdeb532a43882925
zolboo99 <programming.zolboo@gmail.com>
2015-09-03 18:16:09 +0000

    COMMIT INITIALIZE

---
+++ BooleanTest.java
@@ -0,0 +1,8 @@
+public class BooleanTest {
+    public static void main(String[]args){
+        int a=1,b=1,c=1;
+        if (a<=b && b<=c){
+            System.out.println("y");
+        }
+    }
+}
---
+++ StringTest.java
@@ -0,0 +1,11 @@
+public class StringTest {
+    public static void main(String[]args){
+        String n="abcd";
+        int a=16;
+        if (n.substring(0,1)=="a"){
+            System.out.println("EYO");
+        }
+        else System.out.println("wat");
+        System.out.println(String.valueOf(a));
+    }
+}
